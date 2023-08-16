1a9018a29534393911d2f9576d0fbd3997ab8ce1
LH.petillo <130942112+LHPetillo@users.noreply.github.com>
2023-08-16 17:42:18 -0300

    Add files via upload

---
+++ GithubGeradorDeCPF.py
@@ -0,0 +1,11 @@
+import random
+for i in range(10):
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0,9))
+        cpf_enviado_usuario = nove_digitos
+
+    print(cpf_enviado_usuario)
+    contador_regressivo_1 = 10
+
+    
